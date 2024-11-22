04f2773ffe893f45234200688131f0dee3245ef0
Joel Mwala <joelmwala7@gmail.com>
2024-11-22 22:23:21 +0200

    add finance serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Crop, Task  
+from .models import Crop, Task, Finance
 
 class CropSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,4 +10,11 @@ class CropSerializer(serializers.ModelSerializer):
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
+        fields = '__all__'
+
+# finance serializer
+
+class FinanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Finance
         fields = '__all__'
