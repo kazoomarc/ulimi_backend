4efbb6e1e12f027ec281f80ba85f2302ab94ff0d
Joel Mwala <joelmwala7@gmail.com>
2024-11-22 22:53:23 +0200

    import finance viewset

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-from  .views import CropViewSet, TaskViewSet 
+from  .views import CropViewSet, TaskViewSet, FinanceViewSet
 
 # added
 # urlpatterns = router.urls
