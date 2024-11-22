2bb110faef56e866ed22514c8ab56f9dbe6cc2b2
Joel Mwala <joelmwala7@gmail.com>
2024-11-22 22:28:59 +0200

    register finance

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Crop, Task 
+from .models import Crop, Task, Finance
 
 admin.site.register(Crop)
-admin.site.register(Task) 
+admin.site.register(Task)
+admin.site.register(Finance)
