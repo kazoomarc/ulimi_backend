9abf93769749fb206a314949878cb68787a49d45
Joel Mwala <joelmwala7@gmail.com>
2024-11-22 23:35:00 +0200

    register livestockType and livestock

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Crop, Task, Finance, Produce
+from .models import (
+    Crop, Task,
+    Finance, Produce
+    LivestockType, Livestock)
 
 admin.site.register(Crop)
 admin.site.register(Task)
