7c9231a6ef9d41d011c75649b6ccc666c55b81c2
Joel Mwala <joelmwala7@gmail.com>
2024-11-22 23:00:56 +0200

    register Produce model

--- api/admin.py
+++ api/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Crop, Task, Finance
+from .models import Crop, Task, Finance, Produce
 
 admin.site.register(Crop)
 admin.site.register(Task)
 admin.site.register(Finance)
+admin.site.register(Produce)
