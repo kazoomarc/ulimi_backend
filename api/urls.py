a2ea044d73138b7a928e5a75dcafa020067c05b4
Joel Mwala <joelmwala7@gmail.com>
2024-11-22 22:19:20 +0200

    import task view set

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-from  .views import CropViewSet
+from  .views import CropViewSet, TaskViewSet 
 
 # added
-urlpatterns = router.urls
+# urlpatterns = router.urls
