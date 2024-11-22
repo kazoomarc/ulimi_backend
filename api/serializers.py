0a696cf1a8e9a256ccc79492f83a5b7ecf0d3c77
Joel Mwala <joelmwala7@gmail.com>
2024-11-22 22:58:03 +0200

    add produce serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Crop, Task, Finance
+from .models import Crop, Task, Finance, Produce
 
 class CropSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,4 +17,11 @@ class TaskSerializer(serializers.ModelSerializer):
 class FinanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Finance
+        fields = '__all__'
+
+# produce serializer
+
+class ProduceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Produce
         fields = '__all__'
