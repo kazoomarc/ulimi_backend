7eb94b5b0acabb101aec129e571235db39ee9ba8
Joel Mwala <joelmwala7@gmail.com>
2024-11-22 22:20:46 +0200

    migrate Task model

--- api/views.py
+++ api/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
-from .models import Crop
-from .serializers import CropSerializer
+from .models import Crop, Task  
+from .serializers import CropSerializer, TaskSerializer
 
 class CropViewSet(viewsets.ModelViewSet):
     queryset = Crop.objects.all()
     serializer_class = CropSerializer
+
+# task viewSet
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
