6c7524001910f0553527c8af413dae21123d2c71
Joel Mwala <joelmwala7@gmail.com>
2024-11-22 22:22:25 +0200

    add finance model

--- api/models.py
+++ api/models.py
@@ -20,3 +20,18 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+
+# finance model
+
+class Finance(models.Model):
+    TYPE_CHOICES = [
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    ]
+    
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.type} - ${self.amount} on {self.date}"
